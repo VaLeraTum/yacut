5e9f3727310248557fe875f2dc1b24c0fa71b863
ValeraTum <migrivess@gmail.com>
2024-02-16 19:45:41 +0300

    ревью 2

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,6 +1,7 @@
 from random import choices
 from string import ascii_letters, digits
 
+
 def get_unique_short_id():
     for _ in range(5):
         random_url = ''.join(choices(ascii_letters + digits, k=6))
